0f6b9b8b642d228a2d2cd3e59a1834c2047f12b2
Antheny Wells <anthenywells@Go-away.local>
2020-01-19 15:52:48 -0500

    Updates

---
+++ precache-manifest.a2453741323746ce81416e346bf8622c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af538590275255059460f94a15bcc60a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67c4b8ef253ae9929e01",
+    "url": "/static/css/main.cde19557.chunk.css"
+  },
+  {
+    "revision": "9b3b4d223247732e2a0c",
+    "url": "/static/js/2.3cc05626.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3cc05626.chunk.js.LICENSE"
+  },
+  {
+    "revision": "67c4b8ef253ae9929e01",
+    "url": "/static/js/main.a487daa7.chunk.js"
+  },
+  {
+    "revision": "ed05cfd971e34f189a7e",
+    "url": "/static/js/runtime-main.28be1865.js"
+  }
+]);
