b651df33b489cc9351717ae5634834638689c2d9
Antheny Wells <anthenywells@Go-away.local>
2020-01-19 16:04:58 -0500

    Updates

---
+++ precache-manifest.0e5518665d9362f02008e8addee297e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317ef2f8cbe34b0b17a67d0fc68b9c14",
+    "url": "//anthenywells.github.io/react-activity-tracker/index.html"
+  },
+  {
+    "revision": "a72372a3ee59624d5b6d",
+    "url": "//anthenywells.github.io/react-activity-tracker/static/css/main.cde19557.chunk.css"
+  },
+  {
+    "revision": "9b3b4d223247732e2a0c",
+    "url": "//anthenywells.github.io/react-activity-tracker/static/js/2.3cc05626.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "//anthenywells.github.io/react-activity-tracker/static/js/2.3cc05626.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a72372a3ee59624d5b6d",
+    "url": "//anthenywells.github.io/react-activity-tracker/static/js/main.669063b2.chunk.js"
+  },
+  {
+    "revision": "40ddb9dd3b65b686f1a4",
+    "url": "//anthenywells.github.io/react-activity-tracker/static/js/runtime-main.946d7c44.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2453741323746ce81416e346bf8622c.js"
+  "//anthenywells.github.io/react-activity-tracker/precache-manifest.0e5518665d9362f02008e8addee297e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//anthenywells.github.io/react-activity-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
